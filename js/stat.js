fa8145404c7e7e6cb0bc661f3763ed24ebdf0e89
Rustam Sarin <lorienas@gmail.com>
2020-01-26 16:09:18 +0300

    Delete all comments in stat.js

--- js/stat.js
+++ js/stat.js
@@ -12,12 +12,11 @@ var BAR_SPACE = 50;
 var BAR_WIDTH = 40;
 var MAX_BAR_HEIGHT = 150;
 
-// функция для отрисовки поля статистики
 var renderFieldStat = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, FIELD_STAT_WIDTH, FIELD_STAT_HEIGHT);
 };
-// функция для отрисовки текста
+
 var renderText = function (ctx, text, x, y, color, font) {
   ctx.fillStyle = color;
   ctx.font = font;
@@ -41,20 +40,15 @@ var getElementMax = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  // вызов функции отрисовки поля статистики
+
   renderFieldStat(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderFieldStat(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   var fontColor = '#000';
   var fontFamily = '16px PT Mono';
-  // вызов функции отрисовки тайтла
+
   renderText(ctx, 'Ура вы победили!', 150, 30, fontColor, fontFamily);
   renderText(ctx, 'Список результатов:', 150, 46, fontColor, fontFamily);
-
-  // ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  // 50, 100 and 150 - это значения, которые я получу при вычислении их в цикле
-
-  // var players = ['Вы', 'Настя', 'Рустам', 'Popa'];
 
   var maxTime = getElementMax(times);
 
@@ -68,19 +62,4 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillRect(FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * i, FIELD_STAT_END - (MAX_BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (MAX_BAR_HEIGHT * times[i]) / maxTime);
     ctx.fillText(Math.round(times[i]), FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * i, FIELD_STAT_END - (MAX_BAR_HEIGHT * times[i]) / maxTime - GAP);
   }
-  // (MAX_BAR_HEIGHT * times[i]) / maxTime
-  // ctx.fillText(playerName, FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * palyerIndex, FIELD_STAT_HEIGHT - GAP);
-  // ctx.fillRect(FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * palyerIndex, FIELD_STAT_END - 50, BAR_WIDTH, 50);
-
-  // palyerIndex = 1;
-  // playerName = 'Настя';
-
-  // ctx.fillText(playerName, FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * palyerIndex, FIELD_STAT_HEIGHT - GAP);
-  // ctx.fillRect(FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * palyerIndex, FIELD_STAT_END - 100, BAR_WIDTH, 100);
-
-  // palyerIndex = 2;
-  // playerName = 'Рустам';
-
-  // ctx.fillText(playerName, FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * palyerIndex, FIELD_STAT_HEIGHT - GAP);
-  // ctx.fillRect(FIELD_STAT_X + (BAR_SPACE + BAR_WIDTH) * palyerIndex, FIELD_STAT_END - 150, BAR_WIDTH, 150);
 };
