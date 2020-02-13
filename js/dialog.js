92d60d0fe263429a6268960904bd85ea3abcbcc5
Rustam Sarin <lorienas@gmail.com>
2020-02-13 21:48:54 +0300

    create module for moving setup

--- js/dialog.js
+++ js/dialog.js
@@ -10,6 +10,11 @@
   var setupClose = setup.querySelector('.setup-close');
   var userNameInput = setup.querySelector('.setup-user-name');
 
+  var startCoordinateSetup = {
+    x: null,
+    y: null
+  };
+
   userNameInput.addEventListener('invalid', function () {
     if (userNameInput.validity.tooShort) {
       userNameInput.setCustomValidity('Имя должно состоять минимум из 2 символов');
@@ -32,11 +37,15 @@
   var openPopup = function () {
     setup.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
+    startCoordinateSetup.x = setup.offsetLeft;
+    startCoordinateSetup.y = setup.offsetTop;
   };
 
   var closePopup = function () {
     setup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
+    setup.style.top = startCoordinateSetup.y + 'px';
+    setup.style.left = startCoordinateSetup.x + 'px';
   };
 
   setupOpen.addEventListener('click', function () {
@@ -58,4 +67,56 @@
       closePopup();
     }
   });
+
+  var dialogMotion = setup.querySelector('.upload');
+
+  var dialogMotionHandler = function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var mouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var mouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogMotion.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogMotion.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  };
+
+  dialogMotion.addEventListener('mousedown', dialogMotionHandler);
 })();
